4c2264a82e5433e07ee2e51fd5a4103faa9a5e07
vladimirkush <vladimirk@gmail.com>
2016-03-25 16:23:58 +0300

    Initial Commit

---
+++ MemoryGameHW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryGameHW1
+//
+//  Created by Admin on 25/03/2016.
+//  Copyright © 2016 SVTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
